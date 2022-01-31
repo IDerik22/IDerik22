26d079eff341f8922358ef56d6b8895c2a1863cb
IDerik22 <98737025+IDerik22@users.noreply.github.com>
2022-01-31 11:30:13 +0200

    Add files via upload

---
+++ Esimene.py
@@ -0,0 +1,45 @@
+#Harjutus 1
+#Erik Teppan
+#31.01.2022
+'''
+nimi = input ("Lisa oma nimi: ")
+print("Tere", nimi+ "!") #pluss ei lisa tühikut
+vanus = int(input("Lisa oma vanus: "))
+vanuste_vahe = 17 - vanus
+print("Meie vanuste vvahe on:",vanuste_vahe,"aastat")
+'''
+
+'''
+a = 12
+b = 16
+a= b
+s=a*b
+p = 2*(a+b)
+print("Pindala",s,"ruutmeetrit")
+print("Ümbermõõt",p,"meetrit")
+'''
+
+
+'''
+print("Tere!") #Sõne
+print('tere!') #String
+print(2000)  #int
+print(2.5) #float /komaarv
+print('Ütle"Tere maailma"')
+print("Ütle\"Tere maailma\"") #escape string
+print(type(2000)) #Näitab andmetüüpi
+'''
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?" )
+print("""
+  @..@
+ (----)
+( \__/ )
+^^ "" ^^
+""")
+#Sisesta ruudu külje pikkus: 10
+#Vastus:ruudu pindala on 10 cm^2
+
+a = int(print("Sisesta ruudu külje pikkus: "))
+s = a * a
+print("Vastus: ruudu pindala on",s,"cm^2")
