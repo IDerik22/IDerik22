1d10ec04e167bab9e1a298373789ae3ce668916b
IDerik22 <98737025+IDerik22@users.noreply.github.com>
2022-03-02 11:57:46 +0200

    Add files via upload

---
+++ phyton-alused/Töö 01 g2  ET.py
@@ -0,0 +1,43 @@
+# Erik Teppan
+# 02.03.2022
+#IT-21
+
+# Tervitus
+def tere():
+    print("Tere, maailm!")
+
+tere()
+
+# Aastaliblikas
+def liblikas():
+    aasta = 2020
+    liblikas = "teelehe-mosaiikliblikas"
+    lause_keskosa = ". aasta liblikas on "
+    lause = str(aasta)+lause_keskosa+liblikas
+    print(lause)
+
+liblikas()
+
+# Pilved
+def pilved():
+    korgus = float(input("Sisesta pilvede kõrgus (näide 5.4): "))
+    if korgus > 6.0:
+        print("Need on ülemised pilved")
+    else:
+        print("Need ei ole ülemised pilved")
+        
+pilved()
+
+# Bussid
+def bussid():
+    inimesed = int(input("Sisesta inimeste arv: "))
+    istekohad = int(input("Sisesta bussi kohtade arv: "))
+    viimane = inimesed%istekohad
+    if viimane == 0:
+        bussid = inimesed//istekohad
+        viimane = istekohad
+    else:
+        bussid = inimesed//istekohad+1
+    print(f"Busse vaja: {bussid}\nViimases bussis inimesi: {viimane}")
+
+bussid()
---
+++ phyton-alused/Töö 02 g2 ET.py
@@ -0,0 +1,43 @@
+#Erik Teppan
+# 02.03.2022
+#IT-21
+
+import random
+
+# Äratus 2.1
+def aratus():
+    korrad = int(input("Mitu korda on vaja äratada: "))
+    while korrad >= 1:
+        print("Tõuse ja sära!")
+        korrad += -1
+aratus()
+
+# Murelikud Lapsevanemad 2.2
+def mure():
+    ringid = int(input("Mitu ringi läbiti: "))
+    porgandid = 0
+    while ringid >= 1:
+        if ringid%2 == 0:
+            porgandid += ringid
+        ringid += -1
+    print(f"porgandite koguarv on {porgandid}")
+mure()
+
+# Täringud 2.3
+def taringud():
+    kogus = int(input("Mitut täringut lähed vaja: "))
+    for q in range(kogus):
+        vastus = random.randint(1,6)
+        print(vastus)
+taringud()
+
+# Male 2.4
+def male():
+    taisarv = int(input("Mitmes ruut: "))
+    i = 1
+    terad = 1
+    while i <= taisarv-1:
+        terad *= 2
+        i += 1
+    print(f"nisu teri {taisarv}. ruudu eest: {terad}")
+male()
