e05a7d6df8c6165bf5855556c0aa919f975d634e
IDerik22 <98737025+IDerik22@users.noreply.github.com>
2022-03-02 15:25:20 +0200

    Update Töö 03 g2 ET.py

--- phyton-alused/Töö 03 g2 ET.py
+++ phyton-alused/Töö 03 g2 ET.py
@@ -2,54 +2,24 @@
 #02.03.2022
 #IT-21
 
-
-
-
-# Ülikooli vastuvõetud 3.1
-def vastuvoetud():
-    fail1 = open("rebased.txt", encoding="UTF-8")
-    vastuvoetud = []
-    for w in fail1:
-        vastuvoetud.append(int(w))
-    fail1.close()
-    aasta = int(input("millise aasta andmeid vaja läheb (2011-2019): "))
-    aastad = [2011,2012,2013,2014,2015,2016,2017,2018,2019]
-    koht = aastad.index(aasta)
-    andmed = vastuvoetud[koht]
-    print(f"{aasta}. aastal oli vastuvõetuid {andmed}")
-vastuvoetud()
-
-# Vanemate mure 3.2
-def mure():
-    ringid = int(input("Sisestage ringide arv:"))
-    porgandid = 0
-    for q in range(ringid-1):
-        if ringid%2 == 0:
-            porgandid += ringid
-        ringid += -1
-    print(f"porgandite koguarv on {porgandid}")
-mure()
-
-# Sissetulekud 3.3
-def sissetulekud():
-    fail2 = open("konto.txt", encoding = "UTF-8")
-    for e in fail2:
-        arv = float(e)
-        if arv > 0.0:
-            print(arv)
-sissetulekud()
-
-# Jukebox 3.4
-def jukebox():
-    failinimi = input("sisestage failinimi koos laiendiga (näiteks 'jukebox.txt'): ")
-    fail3 = open(failinimi, encoding = "UTF-8")
-    laulud = []
-    jarjekord = 1
-    for t in fail3:
-        print(f"{jarjekord}. {t}")
-        laulud.append(t)
-        jarjekord += 1
-    valik = int(input("vali laulu järjekorra number: "))-1
-    laul = laulud[valik]
-    print(f"Mängitav muusikapala on {laul}")
-jukebox() 
+#3.1
+fail = open("rebased.txt", encoding="UTF-8")
+vastuvoetud = []
+for rida in fail:
+    vastuvoetud.append(int(rida))
+aasta = input("Sisesta, milliste aastate andmeid on vaja: ")
+print(f"{aasta}. aastal oli vastuvõetud{vastuvoetud} [int(aasta[3])-1]")
+#3.2
+ringid = int(input("sisestage ringide arv: "))
+porgand = 0
+for i in range(1,ringid+1):
+    if i%2==0:
+        porgand +=i
+print(f"porganite koguarv on {porgand}")
+#3.3
+fail = open("konto.txt", encoding="UTF-8")
+for rida in fail:
+    if float(rida) > 0:
+        print(rida,end="")
+    print(rida)
+#Rohkem ei jõudnud teen tunnis edasi
