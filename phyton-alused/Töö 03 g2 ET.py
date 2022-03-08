4617a7954428a7ed2b9d39044cdc1f909cb25f95
IDerik22 <98737025+IDerik22@users.noreply.github.com>
2022-03-08 09:54:40 +0200

    Update Töö 03 g2 ET.py

--- phyton-alused/Töö 03 g2 ET.py
+++ phyton-alused/Töö 03 g2 ET.py
@@ -21,4 +21,32 @@ for rida in fail:
     if float(rida) > 0:
         print(rida,end="")
     print(rida)
-#Rohkem ei jõudnud teen tunnis edasi
+#3.4 Minu versioon mis töötab teist moodi õigesti
+import re;
+fpointer = open("jukebox.txt", "r");
+for i in fpointer.readlines():
+   line= re.findall(r'Journey', i);
+   if line:
+    print(i);
+fpointer.close();
+fail1 = input("Sisesta faili nimi: ")
+fail3 = fail1;
+fail1 = open("jukebox.txt")
+lines = fail1.readlines()
+for line in lines:
+     print(line)
+fpointer = open(fail3, "r");
+fail2 = input("Mis laulu soovid?: ")
+for i in fpointer.readlines():
+   line= re.findall(fail2, i);
+   if line:
+    print("Sinu valitud laul on:",i);    
+fpointer.close();
+#3.5
+fail = open("nimed.txt", encoding="UFT-8")
+from datetime import * 
+kp = datetime.now().day
+for i in fail:
+    if jrk==kp:
+        print(i)
+    jrk +=1
