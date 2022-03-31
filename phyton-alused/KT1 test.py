bedc3d4056528034888924e7c6b65b179481f6bd
IDerik22 <98737025+IDerik22@users.noreply.github.com>
2022-03-31 09:24:20 +0300

    Valmis

--- phyton-alused/KT1 test.py
+++ phyton-alused/KT1 test.py
@@ -1,10 +1,12 @@
+#Erik Teppan
+
 from random import *
+import datetime
 import os
 def clear():
     os.system("cls")
 arv = int(input("Mitme tehteni lahendame?\n"))
 clear()
-
 
 def tehe1(arv, oige):
     for i in range (arv):
@@ -19,7 +21,7 @@ def tehe1(arv, oige):
         elif tehe == "-":
             kokku = n1 - n2
             kokku = str(kokku);
-        print(n1,tehe,n2,"=",kokku)
+        print(n1,tehe,n2,"= ?")
         
         kokku1 = input("Sisesta vastus: ")
         print("───────────")
@@ -29,6 +31,39 @@ def tehe1(arv, oige):
         else:
             print("Vale")
         print("───────────")
-    print(f"Õiged {oige}/{arv}")
- 
+    protsent = oige * 100 / arv
+    print(f"{protsent}%")
+    print(f"Õiged {oige}/{arv} ")
+    
+    if protsent >= 90:
+        print("Tubli said 5!")
+    elif protsent >= 85:
+        print("Tubli said 4!")
+    elif protsent >= 65:
+        print("Tubli said 3!")
+    else:
+        print("Said 2!")
+    
+    uuesti = input("Kas soovid uuesti proovida (Ja,Ei)?").upper()
+    if uuesti == "JA":
+        tehe1(arv, 0)
+    elif uuesti == "EI":
+        if protsent <= 61:
+            print("Harjuta veel ja proovi uuesti!!!")
+        elif protsent == 100:
+            paev = datetime.datetime.now()        
+            print(f"""
+
+
+                AUKIRI
+            ────────────────
+            Tubli said 100%
+                  {oige}/{arv}
+            ────────────────
+       {paev}
+
+
+            
+    """)
+    
 tehe1(arv, 0)
